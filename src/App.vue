ce31b62c5987cde08412b5244f9cd341edf00696
Valentina Urquijo <valentinaurquijo@gmail.com>
2024-11-14 11:52:45 -0500

    aaaaaaa

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import headerMainComponent from './components/header/headerMainComponent.vue';
+import landingView from './views/landingView.vue'
 import footerComponent from './components/footerComponent.vue';
 </script>
 
 <template>
   <headerMainComponent></headerMainComponent>
+  <landingView></landingView>
   <footerComponent></footerComponent>
 </template>
 
---
+++ src/views/landingView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container-fluid bg-danger text-white py-5">
+      <div class="row align-items-center">
+        <div class="col-md-6">
+          <h1 class="display-4 fw-bold">TODO TIPO DE RECETAS</h1>
+          <h2 class="fw-light">AL ALCANCE DE TUS MANOS</h2>
+          <p>
+            Publica, edita y califica recetas. Sigue tutoriales paso a paso y comparte tu pasión por la cocina. ¡Inspírate y cocina con confianza!
+          </p>
+          <button class="btn btn-light btn-lg mt-3">Registrarse</button>
+        </div>
+
+        <div class="col-md-6 text-center">
+          <img src="../assets/f_bandejitapaisa.png" alt="Comida" class="img-fluid rounded">
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <style scoped>
+  .bg-danger {
+    background-color: #4f0f15 !important;
+  }
+
+  
+  </style>
+  
